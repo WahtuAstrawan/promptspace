0a7bd22befe3d4ffba34ed4d37e46e5dfc67b7bc
Wahtu Astrawan <chatt699@gmail.com>
2024-04-29 15:40:09 +0800

    chore: initilize nextjs app

---
+++ app/posts/new/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewPostPage = () => {
+  return (
+    <div className='p-12'>NewPostPage</div>
+  )
+}
+
+export default NewPostPage
---
+++ app/posts/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostsPage = () => {
+  return (
+    <div className='p-12'>PostsPage</div>
+  )
+}
+
+export default PostsPage
