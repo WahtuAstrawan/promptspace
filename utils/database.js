d49a3524382f47c72a535ee2d0740069a590d1c0
Wahtu Astrawan <chatt699@gmail.com>
2024-05-15 10:33:12 +0800

    feat: adding auth with google provider and mongoose

---
+++ utils/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const dbConnect = async () => {
+    mongoose.set('strictQuery', true);
+
+    if(isConnected){
+        console.log('MongoDB is already connected');
+        return;
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: "promptopia"
+        })
+
+        isConnected = true;
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error(error);
+    }
+
+
+}
