1000dc53b266f810469a6818700b90b14756dc50
Wahtu Astrawan <chatt699@gmail.com>
2024-05-15 10:32:49 +0800

    feat: adding auth style with google provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,6 +12,7 @@ const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
         <body>
+            <Provider>
             <div className='main'>
                 <div className='gradient' />
             </div>
@@ -20,6 +21,7 @@ const RootLayout = ({ children }) => {
                 <Nav />
                 {children}
             </main>
+            </Provider>
         </body>
     </html>
   )
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,15 +6,18 @@ import { useState, useEffect } from 'react'
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
-    const isUserLoggedIn = true
+    const {data:session} = useSession();
     const [providers, setProviders] = useState(null)
     const [toggleDropdown, setToggleDropdown] = useState(false)
 
     useEffect(() => {
-        (async () => {
+        const setUpProviders = async () => {
             const res = await getProviders();
+
             setProviders(res);
-        })()
+        }
+
+        setUpProviders();
     }, [])
 
   return (
@@ -32,7 +35,7 @@ const Nav = () => {
 
         {/* Dekstop Navigation */}
         <div className='sm:flex hidden'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex gap-3 md:gap-5'>
                     <Link href="/create-prompt" className='black_btn'>
                         Create Post
@@ -42,7 +45,7 @@ const Nav = () => {
                     </button>
                     <Link href="/profile"> 
                         <Image 
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
@@ -72,10 +75,10 @@ const Nav = () => {
 
         {/* Mobile Navigation */}
         <div className='sm:hidden flex relative'>
-            { isUserLoggedIn ? (
+            { session?.user ? (
                 <div className='flex'>
                     <Image 
-                            src="/assets/images/logo.svg"
+                            src={session?.user.image}
                             width={37}
                             height={37}
                             className='rounded-full'
