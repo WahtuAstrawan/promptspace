5dbd71b2b3f5496f0ba2aa1a90f19ddd0832acae
Wahtu Astrawan <chatt699@gmail.com>
2024-05-21 12:16:21 +0800

    feat: adding read and copied prompt

---
+++ app/api/prompt/route.js
@@ -0,0 +1,13 @@
+import { dbConnect } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (req) => {
+    try {
+        await dbConnect();
+        const prompts = await Prompt.find({}).populate('creator');
+
+        return new Response(JSON.stringify(prompts), {status: 200});
+    } catch (error) {
+        return new Response("Failed to fetch prompts", {status: 500}); 
+    }
+}
