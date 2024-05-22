8c48285c88bcb58190a2fb3729ddf483c0ce0040
Wahtu Astrawan <chatt699@gmail.com>
2024-05-22 13:31:14 +0800

    fix: clickable use name func

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -17,23 +17,46 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
     setTimeout(() => setCopied(""), 3500);
   }
 
+  const handleUsernameClick = (creatorId) => {
+
+  }
+
   return (
     <div className='prompt_card'>
       <div className='flex justify-between items-start gap-5'>
-        <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer'>
-          <Image
-            src={post.creator.image}
-            alt='user_image'
-            width={40}
-            height={40}
-            className='rounded-full object-contain'
-          />
+        {pathName === '/profile' && (
+          <div className='flex-1 flex justify-start items-center gap-3'>
+            <Image
+              src={post.creator.image}
+              alt='user_image'
+              width={40}
+              height={40}
+              className='rounded-full object-contain'
+            />
 
-          <div className='flex flex-col'>
-            <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
-            <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
+            <div className='flex flex-col'>
+              <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
+              <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
+            </div>
           </div>
-        </div>
+        )}
+
+        {pathName === '/' && (
+          <div className='flex-1 flex justify-start items-center gap-3 cursor-pointer' onClick={handleUsernameClick(post.creator._id)}>
+            <Image
+              src={post.creator.image}
+              alt='user_image'
+              width={40}
+              height={40}
+              className='rounded-full object-contain'
+            />
+
+            <div className='flex flex-col'>
+              <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
+              <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
+            </div>
+          </div>
+        )}
         <div className='copy_btn' onClick={() => handleCopy()}>
           <Image
             src={copied === post.prompt
@@ -47,7 +70,13 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
       </div>
 
       <p className='my-4 font-satoshi text-sm text-gray-700'>{post.prompt}</p>
-      <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick && handleTagClick(post.tag)}>#{post.tag}</p>
+      {pathName === '/' && (
+        <p className='font-inter text-sm blue_gradient cursor-pointer' onClick={() => handleTagClick && handleTagClick(post.tag)}>#{post.tag}</p>
+      )}
+
+      {pathName === '/profile' && (
+        <p className='font-inter text-sm blue_gradient'>#{post.tag}</p>
+      )}
 
       {session?.user.id === post.creator._id && pathName === '/profile' && (
         <div className='mt-4 flex-end gap-4 border-t border-gray-100 pt-2'>
