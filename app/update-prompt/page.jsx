72c1ef8d93d7dcc2321106bc6f6360df03b48130
Wahtu Astrawan <chatt699@gmail.com>
2024-06-09 14:11:48 +0800

    fix: wrapped use params in suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from 'react'
+import { useState, useEffect, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation';
 
 import Form from '@components/Form';
@@ -57,13 +57,15 @@ const UpdatePrompt = () => {
   }
 
   return (
-    <Form
-        type="Update"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={updatePrompt}
-    />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Form
+          type="Update"
+          post={post}
+          setPost={setPost}
+          submitting={submitting}
+          handleSubmit={updatePrompt}
+      />
+    </Suspense>
   )
 }
 
