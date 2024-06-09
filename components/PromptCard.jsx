a57df91a69ca6c4fb46966ca4dd8b480a62ba90a
Wahtu Astrawan <chatt699@gmail.com>
2024-06-09 14:45:42 +0800

    fix: profile view in user page

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -42,6 +42,23 @@ const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
           </div>
         )}
 
+        {pathName.includes('/profile/') && (
+          <div className='flex-1 flex justify-start items-center gap-3'>
+            <Image
+              src={post.creator.image}
+              alt='user_image'
+              width={40}
+              height={40}
+              className='rounded-full object-contain'
+            />
+
+            <div className='flex flex-col'>
+              <h3 className='font-satoshi font-semibold text-gray-900'>{post.creator.username}</h3>
+              <p className='font-inter text-sm text-gray-500'>{post.creator.email}</p>
+            </div>
+          </div>
+        )}
+
         {pathName === '/' && (
           <div className='flex-1 flex justify-start items-center gap-3'>
             <Image
