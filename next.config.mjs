345f4710453565b86bc82bc0b1a84fc1bbd05137
Wahtu Astrawan <chatt699@gmail.com>
2024-06-13 16:22:28 +0800

    style: for better form ux

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,18 @@
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          hostname: "lh3.googleusercontent.com",
-        },
-      ],
-    },
-    webpack(config) {
-      config.experiments = {
-        ...config.experiments,
-        topLevelAwait: true,
-      };
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    };
+    return config;
+  },
+};
+
+export default nextConfig;
