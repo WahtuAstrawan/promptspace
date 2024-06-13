a96d9387651f4ae7d8d59adcb5e6863f644399e4
Wahtu Astrawan <chatt699@gmail.com>
2024-06-13 16:42:10 +0800

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -6,13 +6,6 @@ const nextConfig = {
       },
     ],
   },
-  webpack(config) {
-    config.experiments = {
-      ...config.experiments,
-      topLevelAwait: true,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
